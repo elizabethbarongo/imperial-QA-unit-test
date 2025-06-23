94b489515798d10fa357b17e30a62271aad0d09e
Elizabeth Barongo <elizabethmoraab@gmail.com>
2025-06-23 11:02:10 +0300

    deleted files for unit tests

--- login.js
+++
@@ -1,9 +0,0 @@
-function login(email, password) {
-    if (email === 'admin@recycleco.com' && password === 'adminpass') {
-      return { token: 'valid-token', role: 'admin' };
-    } else if (email === 'user@recycleco.com' && password === 'userpass') {
-      return { token: 'valid-token', role: 'user' };
-    }
-    return null;
-  }
-  module.exports = login;
