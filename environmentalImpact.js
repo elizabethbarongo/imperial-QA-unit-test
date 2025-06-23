46a5bc143e27e2443cf3c86d9340df88349391c7
elizabethbarongo <elizabethmoraab@gmail.com>
2025-06-23 11:49:57 +0300

    Revert "added files with test scripts for a dashboard"

--- environmentalImpact.js
+++
@@ -1,8 +0,0 @@
-function getEnvironmentalMetrics() {
-    return {
-      co2SavedTonnes: 1250,
-      waterSavedLiters: 500000,
-      energySavedKWh: 30000,
-    };
-  }
-  module.exports = getEnvironmentalMetrics;
